581711181f3c01e92167c5365bf18d81889e47ca
Jack J <69510203+JackJey@users.noreply.github.com>
2021-12-01 11:33:26 +0900

    [v3] up

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("."));
 
 app.get("/", async (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
